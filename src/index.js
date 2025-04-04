93da683a82577dad011207525fc4b7ec1b3d772d
Nypsus <bachatadance@protonmail.com>
2025-04-04 22:08:18 +0200

    Add video and update code

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';  // Cette ligne doit être valide avec React 18
 import App from './App';
 import './index.css'; // Ajoute cette ligne dans ton fichier JavaScript
+import './App.css'; 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
