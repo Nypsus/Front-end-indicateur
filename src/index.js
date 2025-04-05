12901c0e5a1bf78942e0a7a76883b51c13f97151
Nypsus <bachatadance@protonmail.com>
2025-04-05 17:24:44 +0200

    depaclement fichier public

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';  // Cette ligne doit être valide avec React 18
 import App from './App';
-import './index.css'; // Ajoute cette ligne dans ton fichier JavaScript
-import './App.css'; 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
