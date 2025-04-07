e319d5ad1c1757bbfaa42c6ee2b6fcf571a5736c
Nypsus <bachatadance@protonmail.com>
2025-04-07 22:47:27 +0200

    Ajout des fichiers générés par le build sans le dossier build

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';  // Cette ligne doit être valide avec React 18
+import App from './App';
+import './index.css'; // Ajoute cette ligne dans ton fichier JavaScript
+import './App.css'; 
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
